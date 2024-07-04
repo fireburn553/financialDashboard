44aa9e2b43f312870b834ad08cce21f2acb4d4bf
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-07-04 23:26:45 +0800

    remove the ppr incremental to the next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
- 
-const nextConfig = {
-    experimental: {
-      ppr: 'incremental',
-    },
-  };
-   
-  module.exports = nextConfig;
+
+const nextConfig = {};
+
+module.exports = nextConfig;
